936a28dcbaefb451badc7f5dbf6170c728b0fa8e
srinivas s g <srinivasvasu2001@gmail.com>
2026-06-25 00:09:37 +0530

    Added Confirm and Prompt Alert handling

--- pages/AlertPage.ts
+++ pages/AlertPage.ts
@@ -1,31 +1,50 @@
-import { Page } from '@playwright/test';
+import { Page, expect, Locator } from '@playwright/test';
 
 export class AlertPage {
-
+    private confirmResult: Locator;
     constructor(private page: Page) {
-
+        this.confirmResult = page.locator('#demo');
     }
-
     async navigate() {
-
         await this.page.goto(
             'https://testautomationpractice.blogspot.com/'
         );
-
     }
 
     async handleSimpleAlert() {
-
         this.page.on('dialog', async dialog => {
-
             console.log(dialog.message());
-
             await dialog.accept();
-
         });
-
         await this.page.locator('#alertBtn').click();
-
     }
 
+    async acceptConfirmationAlert() {
+        this.page.on('dialog', async dialog => {
+            console.log(dialog.message());
+            await dialog.accept();
+        });
+        await this.page.locator('#confirmBtn').click();
+    }
+
+    async dismissConfirmationAlert() {
+    this.page.on('dialog', async dialog => {
+        console.log(dialog.message());
+        await dialog.dismiss();
+    });
+    await this.page.locator('#confirmBtn').click();
+    }
+
+    async handlePromptAlert() {
+    this.page.on('dialog', async dialog => {
+        console.log(dialog.message());
+        await dialog.accept('Srinivas');
+    });
+    await this.page.locator('#promptBtn').click();
+    }
+
+    async verifyPromptMessage() {
+    await expect(this.confirmResult)
+        .toContainText('Srinivas');
+    }
 }
